69979d9478b2cf2b810096295fb0151c46891804
ivan1986mrpl <121858281+ivan1986mrpl@users.noreply.github.com>
2025-08-20 12:45:59 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  if (!Array.isArray(matrix) || matrix.length === 0) {
+    return [];
+  }
+
+  return matrix.reduce((result, row, index) => {
+    if (index % 2 === 0) {
+      return result.concat(row);
+    }
+    return result.concat(row.reverse());
+  }, []);
 };
